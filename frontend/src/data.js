ca68918fe9418eb7cc00e0bf714bfefbabca3a12
AlexGralewski <gralewski.aleksander@gmail.com>
2021-04-08 21:58:30 +0200

    Initial commit

---
+++ frontend/src/data.js
@@ -0,0 +1,12 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'T-shirt',
+      price: 50,
+      image: '/image.jpg',
+      rating: 5,
+      description: 'This is a description of a product'
+    }
+  ]
+}
