4621f4a17834584306aacd532db02533e9152b64
AlexGralewski <gralewski.aleksander@gmail.com>
2021-04-08 21:55:50 +0200

    Initial commit

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Product = props => (
+  <div className="product">
+    <img className="product-image" src="./assests" alt="Product Photo" />
+    <div className="product-name"></div>
+    <div className="product-Price"></div>
+  </div>
+)
