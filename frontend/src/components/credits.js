99edc0289d2faa1d1c256f02fcfc6d103f840524
AlexGralewski <gralewski.aleksander@gmail.com>
2021-04-09 20:21:15 +0200

    Initial commit

---
+++ frontend/src/components/credits.js
@@ -0,0 +1,43 @@
+const credits = [
+  {
+    name: '',
+    author: 'Photo by Spencer Selover from Pexels',
+  },
+  {
+    name: '',
+    author: 'Photo by Wendy Wei from Pexels',
+  },
+  {
+    name: '',
+    author: 'Photo by Rahul Shah from Pexels',
+  },
+  {
+    name: '',
+    author: 'Photo by Mnz from Pexels',
+  },
+  {
+    name: '',
+    author: 'Photo by Godisable Jacob from Pexels',
+  },
+  {
+    name: '',
+    author: 'Photo by W R from Pexels',
+  },
+  {
+    name: '',
+    author: 'Photo by Ksenia Chernaya from Pexels',
+  },
+  {
+    name: '',
+    author: '',
+  },
+  {
+    name: '',
+    author: '',
+  },
+  {
+    name: '',
+    author: '',
+  },
+
+]
