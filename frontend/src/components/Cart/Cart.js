a18273b9169644946e7f0d9dd23bdf9369690606
AlexGralewski <gralewski.aleksander@gmail.com>
2021-04-19 23:05:00 +0200

    Minor changes

--- frontend/src/components/Cart/Cart.js
+++ frontend/src/components/Cart/Cart.js
@@ -1,25 +1,46 @@
-import React from "react"
-import data from "../../data"
-import CartProduct from "./CartItem"
+import React from "react";
+import data from "../../data";
+import CartProduct from "./CartItem";
 
 const Cart = () => {
-
-  return(
+  return (
     <div className="cart">
       <div className="cart-main">
         <h1>Cart</h1>
         <div className="cart-items">
-          <CartProduct image={data.products[0].image} name={data.products[0].name} price={data.products[0].price}/>
+          <CartProduct
+            image={data.products[0].image}
+            name={data.products[0].name}
+            price={data.products[0].price}
+            size="M"
+          />
+          <CartProduct
+            image={data.products[1].image}
+            name={data.products[1].name}
+            price={data.products[1].price}
+            size="M"
+          />
         </div>
       </div>
       <div className="cart-summary">
-        <div className="products-sum"></div>
-        <div className="products-delivery"></div>
+        <div className="products-sum">
+          <span>Product price</span>
+          <span>{data.products[0].price}$</span>
+        </div>
+        <div className="delivery">
+          <span>Delivery</span>
+          <span>From 0,00</span>
+        </div>
+        <hr />
+        <div className="cart-total">
+          <span>Total</span>
+          <span>50$</span>
+        </div>
         <button>Go to checkout</button>
       </div>
       <div></div>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
--- frontend/src/components/Cart/CartItem.js
+++ frontend/src/components/Cart/CartItem.js
@@ -1,26 +1,29 @@
-import React from "react"
+import React from "react";
 
-const Product = props => {
-  const {name, price, image, size} = props
+const Product = (props) => {
+  const { name, price, image, size } = props;
 
-  return(
+  return (
     <div className="cart-product">
-      <div className="cp-image" style={{backgroundImage:`url(${image})`}} />
-      <div className="cp-info">
-        <div className="cp-name">{name}</div>
-        <div className="cp-size">{size}</div>
-        <div className="cp-qty">1</div>
-
-      </div>
-      <div>
-        <button className="cp-remove">
-          <i className="fas fa-trash"></i>
-          <p>Remove</p>
-        </button>
-        <div className="cp-price">{price}$</div>
+      <div className="cp-image" style={{ backgroundImage: `url(${image})` }} />
+      <div className="cp-container">
+        <div className="cp-top">
+          <div className="cp-info">
+            <h2>{name}</h2>
+            <p>Size: {size}</p>
+          </div>
+          <button className="cp-remove">
+            <i className="fas fa-trash"></i>
+            <p>Remove</p>
+          </button>
+        </div>
+        <div className="cp-bottom">
+          <p>1</p>
+          <p>{price}$</p>
+        </div>
       </div>
     </div>
-)}
+  );
+};
 
-
-export default Product
+export default Product;
